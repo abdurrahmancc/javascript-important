d1467f30653e8009a66a03fbe896e472852c1b8b
abdurrahmancc <personalbusinessmail63@gmail.com>
2022-02-04 19:40:49 +0600

    love calculetor

---
+++ love-calculetor.js
@@ -0,0 +1,8 @@
+function calculetor(name1, name2) {
+  let number = Math.random() * 100;
+  number = Math.floor(number) + 1;
+  let love = name1 + " and " + name2 + "'s love percentage " + number + "%";
+  return love;
+}
+let names = calculetor("farhan", "faria");
+console.log(names);
